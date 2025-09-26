d4a295d756c616afa2fe91f1c8ef34bed7debf77
pedrohvn8n-dotcom <pedrohvn8n@gmail.com>
2025-09-26 19:07:30 -0300

    Corrigir erro de sintaxe na linha 990

--- src/components/NovaOS.tsx
+++ src/components/NovaOS.tsx
@@ -990,7 +990,7 @@ export default function NovaOS({ tenant, onBack }: NovaOSProps) {
                   onBlur={() => handleFieldBlur('telefone_cliente')}
                   }`}
                 />
-                {phoneError && (
+                />
                   <p className="mt-1 text-sm text-red-600 flex items-center space-x-1">
                     <svg className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
