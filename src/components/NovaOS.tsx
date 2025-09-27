0a723c0463a592f2820c1055a0c20f3b500c7f75
pedrohvn8n-dotcom <pedrohvn8n@gmail.com>
2025-09-27 09:09:34 -0300

    Centralizar texto do label N° OS

--- src/components/NovaOS.tsx
+++ src/components/NovaOS.tsx
@@ -880,7 +880,7 @@ export default function NovaOS({ tenant, onBack }: NovaOSProps) {
               </div>
               
               <div className="text-right">
-                <label className="block text-sm font-medium text-gray-700 mb-2 text-left">N° OS</label>
+                <label htmlFor="num_os" className="block text-sm font-medium text-gray-700 mb-2 text-center">
                 <input
                   type="number"
                   value={nextNumOS}
